c8d67a6b08663b027fd9a4fe79e0e96ed6486277
yunlee <yun22490315@naver.com>
2022-09-22 23:07:52 +0900

    wetube-clone_router1

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-
-const logger = morgan("dev");
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.get("/", handleHome);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
